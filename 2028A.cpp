9d5464bac928bc24ee4b98ad81010d5f8bf5ff33
SamratEmily <nayakemily50@gmail.com>
2024-11-13 18:40:50 +0600

    Solved fair distribution

---
+++ 2028A.cpp
@@ -0,0 +1,20 @@
+#include "bits/stdc++.h"
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n, a, b;
+        cin >> n >> a >> b;
+
+        string st;
+        cin >> st;
+    }
+
+    return 0;
+}
